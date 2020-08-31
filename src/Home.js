7a9b7b65881ada16d53763089377aa71fe63f7f4
Ibrahim <ibrahim.s.amin@gmail.com>
2020-08-31 23:33:27 +1000

    date picker and banner completed

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Home.css';
+import Banner from './Banner';
 
 function Home() {
-   return <div className='home'></div>;
+   return (
+      <div className='home'>
+         <Banner />
+         <div className='home__section'></div>
+      </div>
+   );
 }
 
 export default Home;
