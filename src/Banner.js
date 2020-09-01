ced70f0716df754e63ef3e6b559d8301769a8f03
Ibrahim <ibrahim.s.amin@gmail.com>
2020-09-01 21:35:08 +1000

    airbnb-clone completed

--- src/Banner.js
+++ src/Banner.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import './Banner.css';
 import { Button } from '@material-ui/core';
 import Search from './Search';
+import { useHistory } from 'react-router-dom';
 
 function Banner() {
+   const history = useHistory();
    const [showSearch, setShowSearch] = useState(false);
 
    return (
       <div className='banner'>
          <div className='banner__search'>
             {showSearch && <Search />}
+
             <Button
                onClick={() => {
                   setShowSearch(!showSearch);
@@ -23,7 +26,10 @@ function Banner() {
          <div className='banner__info'>
             <h1> Get out and stretch your imagination</h1>
             <h5> Plan a different kind of getway</h5>
-            <Button variant='outlined'> Explore Nearby</Button>
+            <Button onClick={() => history.push('/search')} variant='outlined'>
+               {' '}
+               Explore Nearby
+            </Button>
          </div>
       </div>
    );
--- src/Search.js
+++ src/Search.js
@@ -5,8 +5,10 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
 import PeopleIcon from '@material-ui/icons/People';
 import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 function Search() {
+   const history = useHistory();
    const [startDate, setStartDate] = useState(new Date());
    const [endDate, setEndDate] = useState(new Date());
 
@@ -29,7 +31,7 @@ function Search() {
             <PeopleIcon />
          </h2>
          <input min={0} defaultValue={2} type='number' />
-         <Button>Search AirBnB</Button>
+         <Button onClick={() => history.push('/search')}> Search AirBnB</Button>
       </div>
    );
 }
