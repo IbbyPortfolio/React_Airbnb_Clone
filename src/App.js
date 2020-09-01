ad352b3e23f1c660d40d4690bc1c4ee3086eca60
Ibrahim <ibrahim.s.amin@gmail.com>
2020-09-01 21:20:52 +1000

    SaerchResult page completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-
 import './App.css';
 import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SearchPage from './SearchPage';
 
 function App() {
    return (
       <div className='app'>
-         <Header />
-         <Home />
-         <Footer />
+         <BrowserRouter>
+            <Header />
+            <Switch>
+               <Route path='/search'>
+                  <SearchPage />
+               </Route>
+               <Route path='/'>
+                  <Home />
+               </Route>
+            </Switch>
+            <Footer />
+         </BrowserRouter>
       </div>
    );
 }
